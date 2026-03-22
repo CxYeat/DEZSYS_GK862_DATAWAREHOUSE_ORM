769616d8d63abe2ab777d30a818e96f3b666a0fb
CxYeat <aronhandan2008@gmail.com>
2026-03-22 21:26:19 +0100

    Updated the project structure for a cleaner look

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,5 +1,7 @@
-package org.example;
+package org.example.controller;
 
+import org.example.user.UserRepository;
+import org.example.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
